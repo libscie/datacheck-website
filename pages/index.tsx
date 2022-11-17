1b4eb6307697ce6d6a6c367640c208fab918fec2
Chris Hartgerink <chris@libscie.org>
2022-11-17 09:54:38 +0100

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Hero from './components/Hero'
+import LogoCloud from './components/LogoCloud'
+
+
 
 export default function Home() {
   return (
@@ -11,7 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+<Hero />
+<LogoCloud />
+      {/* <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -65,7 +71,7 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
